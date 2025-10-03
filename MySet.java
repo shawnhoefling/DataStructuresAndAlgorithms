5090889f33447edf8be327912e2f50b6bbf549c5
shawnhoefling <shawnhoefling@gmail.com>
2025-10-03 12:33:02 -0400

    Initial commit

---
+++ MySet.java
@@ -0,0 +1,59 @@
+
+/*************************************************************************
+ *
+ *  Pace University
+ *  Spring 2024
+ *  Data Structures and Algorithms
+ *
+ *  Course: CS 241
+ *  Team authors: PUT THE NAMES OF ALL YOUR TEAM MEMBERS HERE
+ *  External Collaborators: PUT THE NAME OF ANY COLLABORATORS OUTSIDE YOUR TEAM HERE, IF NONE, PUT NONE
+ *  References: PUT THE LINKS TO YOUR SOURCES HERE
+ *
+ *  Assignment: PUT THE ASSIGNMENT NUMBER HERE
+ *  Problem: PUT THE MAIN PROBLEM NAME HERE
+ *  Description: PUT A BRIEF DESCRIPTION HERE
+ *
+ *  Input:
+ *  Output:
+ *
+ *  Visible data fields:
+ *  COPY DECLARATION OF VISIBLE DATA FIELDS HERE
+ *
+ *  Visible methods:
+ *  COPY SIGNATURE OF VISIBLE METHODS HERE
+ *
+ *
+ *   Remarks
+ *   -------
+ *
+ *   PUT ALL NON-CODING ANSWERS HERE
+ *
+ *
+ *************************************************************************/
+
+import java.util.*;
+
+public class MySet<T>{
+    //data fields
+    private ArrayList<T> myList;
+    
+    // constructors
+    MySet(){
+        // CODE HERE
+        
+    }
+    // other methods
+    public void add(T item){
+        // CODE HERE
+    }
+    public void remove(T item){
+        // CODE HERE
+    }
+    public Boolean membership(T item){
+        // CODE HERE
+    }
+    public String toString(){
+        // CODE HERE
+    }
+}
