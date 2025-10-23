3f36fd1e35db50eccb133d387442d434521bae86
shawnhoefling <shawnhoefling@gmail.com>
2025-10-23 11:36:21 -0400

    Finalizing the implemenation of a stack with testing

--- src/MoveToFront.java
+++ src/MoveToFront.java
@@ -59,31 +59,43 @@ public class MoveToFront<T>{
     private LinkedList<T> myList; // declartaion of LinkedList called myList 
     
     // constructors
+
+    // TODO: Double check implementation of linked list, especially in comparison to array lists. 
+
     MoveToFront(){
         this.myList = new LinkedList<T>(); // initialization of LinkedList myList
     }
 
     // other methods
 
+    // TODO: Double check Pop method, ensure this is giving correct output.
+
+    /**
+     * @param item
+     */
     public void push(T item){ // add item to list, if list contains item, pop it
-        if (!contains(item)) { // if the list does NOT contain item, push it
-            myList.push(item);
+        if (myList.contains(item)) { // if the list does contain item, remove (or pop) it
+            myList.remove(item);
         }
-        else {
-            myList.pop(); // this will pop or "remove" it from the list, if there are previous occurences 
-        }
+        
+        myList.push(item);  // if list doesnt contain item, pushes it to stack
+        
     }
 
-    public void pop(T item){ // removes last pushed item
-        if (!myList.isEmpty()) { // if the list is not empty at the top, pop it
-            myList.pop(); // see about having a counter for the top of stack, or look into finding top of stack. maybe two stacks.
-        }
-        else {
+    // TODO: rework Pop, it needs to remove from the last element added. think in terms of stacks!!
 
+    public T pop(){ // removes last pushed item
+        /* 
+        if (myList.isEmpty()) { // if the list is empty
+            myList.pop(); // see if a return statemnet would be more fitting here
+        } else {
+            myList.pop();
         }
+            */
+        return myList.pop();
     }
 
-    public Boolean contains(T item){ // if the list contains the integer, returns true, if not false.
+    public Boolean contains(T item){ // if the list contains the item, returns true, if not false.
         return myList.contains(item); 
     }
 
@@ -91,6 +103,7 @@ public class MoveToFront<T>{
         return myList.isEmpty();   
     }
 
+    @Override
      public String toString(){
         return myList.toString(); // returns a string with the list of elements.
     }
--- src/TestMTF.java
+++ src/TestMTF.java
@@ -3,64 +3,50 @@ import java.util.*;
 public class TestMTF {
     public static void main(String[] args) {
 
-        Scanner input = new Scanner(System.in); // scanner to take in user input for last step. 
+        MoveToFront<String> mtf = new MoveToFront<>(); // using MoveToFront object to create set of items
 
-        MoveToFront<Integer> setA = new MoveToFront<>(); // using MySet object to create Set A
-
-        // TODO: Completely redo and double check the test, it was carried over from last assignment so double check everything.
+        // initalizing the stack
     
+        System.out.println("Current stack: " + mtf.toString());
+        System.out.println("Adding items to the stack..."); 
 
-        System.out.println("Adding integers to Set A:"); // hard coding the adding to set A with differing integers per instructions.
+        // testing pushing items to the stack
 
-        setA.push(7);
-        setA.push(4);
-        setA.push(9);
-        setA.push(11);
+        mtf.push("A");
+        mtf.push("B");
+        mtf.push("C");
+        mtf.push("D");
+        mtf.push("E");
+        mtf.push("F");
+
+        // displaying current stack (after pushes)
+
+        System.out.println("Current stack: " + mtf.toString());
         
+        // popping items from stack
 
-        System.out.println("Set A: " + setA.toString()); // displaying using toString elements of the set that the user entered 
+        mtf.pop();
+        mtf.pop();
 
-        setA.pop(4);
-        setA.pop(9);
+        // displaying current stack (after pops)
+
+        System.out.println("Stack after pop: " + mtf.toString()); // displaying using toString elements of the set that the user entered 
     
+        // testing contains method
 
-        System.out.println("Set A with removals: " + setA.toString()); // displaying the updated set with 2, 9, and 7 removed. 
+        System.out.println("Does the stack contain F? = " + mtf.contains("F"));
+        System.out.println("Doest the stack contain A? = " + mtf.contains("A"));
 
-        System.out.println("Enter a number you want to check is or is not in the set: ");  // checking a number a user inputs
-        int userNumber = input.nextInt(); // takes in user input
+        // popping the rest of the list
 
+        System.out.println("Popping the rest of the stack...");
 
-        System.out.println(setA.contains(userNumber)); // compares the user number with the set to see if what they entered is or is not in the set and returns true or false
+        mtf.pop();
+        mtf.pop();
+        mtf.pop();
+        mtf.pop();
 
-        input.close(); // closing scanner
-
-        // this section is the extra credit section of the testing
-        
-/* 
-        // two new sets for testing union, intersection, and difference methods
-        MySet<Integer> setB = new MySet<>();
-        MySet<Integer> setC = new MySet<>();
-
-        System.out.println("Adding integers to set B: ");
-        setB.add(4);
-        setB.add(7);
-        setB.add(9);
-        setB.add(11);
-
-        System.out.println("Set B: " + setB.toString()); // displaying set Butilizing toString
-
-        System.out.println("Adding integers to set C: ");
-        setC.add(11);
-        setC.add(5);
-        setC.add(9);
-        setC.add(6);
-
-        System.out.println("Set C: " + setC.toString()); // displaying set C utilizing toString
-
-*/
-
- 
-
+        System.out.println("Is the stack empty? = " + mtf.isEmpty());
         
     }
 }
